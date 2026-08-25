900e65a8840fc67782ba49e25acfe2a339d81d10
Amakedo <korole793@gmail.com>
2026-08-25 15:19:29 +0300

    ы

--- CarSell/Models/Car.cs
+++ CarSell/Models/Car.cs
@@ -5,7 +5,7 @@ namespace CarSell.Models
     public class Car
     {
         public int Id { get; set; }
-
+        public int Number{ get; set; }
         public string Brand { get; set; } = string.Empty;
         public string Model { get; set; } = string.Empty;
         public int Year { get; set; }
